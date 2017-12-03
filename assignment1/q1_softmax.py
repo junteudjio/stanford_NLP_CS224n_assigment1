76a2410df74dc2ed0c773c738fab74356926508c
teudjio <teudjiombativou@gmail.com>
2017-12-03 09:43:36 +0100

    implement and test q1_softmax.py

--- assignment1/q1_softmax.py
+++ assignment1/q1_softmax.py
@@ -31,12 +31,38 @@ def softmax(x):
     if len(x.shape) > 1:
         # Matrix
         ### YOUR CODE HERE
-        raise NotImplementedError
+
+        # we first remove the max for each row before computing the softmax
+        # we do this for numericall stability in case of large values
+        # and we are allowed to because softmax is invariant to translations
+        per_row_max = np.max(x, axis=1).reshape(-1, 1)
+        x = x - per_row_max
+
+        x = np.exp(x)
+        per_row_sum = np.sum(x, axis=1).reshape(-1, 1)
+        x = 1. * x / per_row_sum  # we multiply be 1. to ensure float division
+
         ### END YOUR CODE
     else:
         # Vector
         ### YOUR CODE HERE
-        raise NotImplementedError
+
+        # this  means that x's shape is something like (34,)
+        # what we do is to reshape it as (1,34) matrix and perfom the exact same steps above
+        # and reshape it back to its original shape
+        x = x.reshape(1, -1)
+
+        # exact same steps as above
+        # NOTE: we can't factorized code since the assignments requires to write code in specific area
+        per_row_max = np.max(x, axis=1).reshape(-1, 1)
+        x = x - per_row_max
+        x = np.exp(x)
+        per_row_sum = np.sum(x, axis=1).reshape(-1, 1)
+        x = 1. * x / per_row_sum  # we multiply be 1. to ensure float division
+
+        # we reshape x to its original shape
+        x = x.reshape(orig_shape)
+
         ### END YOUR CODE
 
     assert x.shape == orig_shape
@@ -78,7 +104,17 @@ def test_softmax():
     """
     print "Running your tests..."
     ### YOUR CODE HERE
-    raise NotImplementedError
+    test1 = softmax(np.array([0, 0, 0, 0]))
+    ans1 = np.array([0.25, 0.25, 0.25, 0.25])
+    assert np.allclose(test1, ans1, rtol=1e-05, atol=1e-06)
+
+    test2 = softmax(np.array([[0, 0, 0, 0]]))
+    ans2 = np.array([[0.25, 0.25, 0.25, 0.25]])
+    assert np.allclose(test2, ans2, rtol=1e-05, atol=1e-06)
+
+    test3 = softmax(np.array([[0]]))
+    ans3 = np.array([[1.]])
+    assert np.allclose(test3, ans3, rtol=1e-05, atol=1e-06)
     ### END YOUR CODE
 
 
