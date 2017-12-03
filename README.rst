bda89deb56fb67de0ce85c63c5b210b20ab81f6e
teudjio <teudjiombativou@gmail.com>
2017-12-03 05:50:54 +0100

    init commit; starter code

---
+++ README.rst
@@ -0,0 +1,10 @@
+README: (TO COMPLETE)
+================================================================================
+
+1- Motivations:
+    - Improve my theoretical and practical knowledge of Tensorflow, Deep Learning, NLP
+      by working on cs224n maths and programming problem sets.
+
+2- Setup:
+
+3- Doc:
---
+++ setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='assignment1',
+    version='0.1.0',
+    description='My solutions to Stanford University NLP and Deep Learning cs224 class',
+    long_description=readme,
+    author='Junior Teudjio Mbativou',
+    author_email='teudjiombativou@gmail.com',
+    url='https://github.com/teudjio',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+        'nose',
+        'sphinx',
+        'matplotlib',
+        'scipy',
+        'numpy',
+        'sklearn',
+    ]
+)
+
