e9f8ccb58198b71f1216074438a98518c08f0075
teudjio <teudjiombativou@gmail.com>
2018-01-04 07:52:34 +0100

    add readme, remove unneeded stuffs

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@
 from setuptools import setup, find_packages
 
 
-with open('README.rst') as f:
+with open('README.md') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open('LICENSE.md') as f:
     license = f.read()
 
 setup(
